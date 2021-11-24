3f0511dfb2d1877c68dc0344f83e0a0bf9148860
Mysia Santana <mysiasantana@gmail.com>
2021-11-23 17:44:04 -0700

    created folders for _slug.vue

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,11 +33,11 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    '@nuxtjs/tailwindcss','@nuxtjs/vuetify','@nuxt/image',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
+  modules: ['@nuxt/content',
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
