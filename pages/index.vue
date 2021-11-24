3494d79e85b8aa7c320309607409d49a8a786fdb
Mysia Santana <mysiasantana@gmail.com>
2021-11-23 19:04:43 -0700

    edited index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,35 @@
 <template>
-  <Tutorial/>
+  <nav>
+    <h2>Articles on Dogs</h2>
+    <v-card>
+      <ul>
+        <li v-for="post in posts" :key="post.slug">
+          <nuxt-link :to="`gallery/${post.slug}`">{{ post.title }}</nuxt-link>
+        </li>
+      </ul>
+    </v-card>
+    <h2>Four Seasons</h2>
+    <v-card>
+      <ul>
+        <li v-for="recipe in recipes" :key="recipe.slug">
+          <nuxt-link :to="`recipes/${recipe.slug}`">{{
+            recipe.recipeName
+          }}</nuxt-link>
+        </li>
+      </ul>
+    </v-card>
+  </nav>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $content }) {
+    const posts = await $content("gallery").fetch();
+    const recipes = await $content("recipe").fetch();
+    return {
+      posts,
+      recipes,
+    };
+  },
+};
 </script>
