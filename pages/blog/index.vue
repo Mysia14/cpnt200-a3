a0fe9e197923c7153347a929ef08df3ef10e4e33
Mysia Santana <mysiasantana@gmail.com>
2021-11-24 20:40:16 -0700

    added index.vue in pages

---
+++ pages/blog/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-main>
+    <header>
+      <Header :title="info.title" :subHeading="info.tagline" />
+    </header>
+    <section>
+      <h2>The beauty of the Seasons</h2>
+      <ul>
+        <li>{{ topics[0] }}</li>
+        <li>{{ topics[1] }}</li>
+        <li>{{ topics[2] }}</li>
+      </ul>
+    </section>
+    <section>
+      <h2>Credits</h2>
+      <p>{{ references.name }}, {{ references.location }}</p>
+    </section>
+    <footer>
+      <p>&copy; 2021 Mysia Santana</p>
+    </footer>
+  </v-main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      info: {
+        title: "photos",
+        author: "mysia",
+        tagline: "the beautiful nature",
+      },
+      references: {
+        name: "nature",
+        location: "Canada",
+      },
+      topics: ["memories", "album", "stories"],
+    };
+  },
+};
+</script>
---
+++ pages/recipes/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-main>
+    <header>
+      <Header :title="info.title" :subHeading="info.author" />
+    </header>
+    <section>
+      <h2>My Gallery</h2>
+      <p>here are some recipes I make</p>
+      <ul>
+        <li>{{ topics[0] }}</li>
+        <li>{{ topics[1] }}</li>
+        <li>{{ topics[2] }}</li>
+      </ul>
+    </section>
+    <section>
+      <h2>Memories</h2>
+      <p>{{ references.name }}, {{ references.location }}</p>
+    </section>
+    <footer>
+      <p>&copy; 2021 Mysia Santana</p>
+    </footer>
+  </v-main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      info: {
+        title: "food stuff",
+        author: "mysia",
+        tagline: "beautiful memories",
+      },
+      references: {
+        name: "Album",
+        location: "Calgary",
+      },
+      topics: ["Canada", "immigration", "photos"],
+    };
+  },
+};
+</script>
